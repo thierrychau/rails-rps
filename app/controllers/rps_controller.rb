6a34e00b30d7cef110ac03e55af79f87c0103058
Thierry Chau <thierry.chau@gmail.com>
2023-09-25 21:08:30 +0000

    added rock, paper, and scissors functions

--- app/controllers/rps_controller.rb
+++ app/controllers/rps_controller.rb
@@ -3,9 +3,38 @@ class RpsController < ApplicationController
     render({ :template => "game_templates/rules_rps" })
   end
 
+  RPS = ["rock", "paper", "scissors"]
+
+  def rps_game(user_rps, computer_rps)
+    if user_rps == computer_rps
+      return "tied"
+    elsif (user_rps == "rock" && computer_rps == "scissors") || (user_rps == "scissors" && computer_rps == "paper") || (user_rps == "paper" && computer_rps == "rock")
+      return "won"
+    else
+      return "lost"
+    end
+  end
+
   def rock
     @user_move = "rock"
-    @random_move = ["rock", "paper", "scissors"].sample
+    @computer_rps = RPS.sample
+    @rps_outcome = rps_game(@user_move, @computer_rps)
+
+    render({ :template => "game_templates/play_rps" })
+  end
+
+  def paper
+    @user_move = "paper"
+    @computer_rps = RPS.sample
+    @rps_outcome = rps_game(@user_move, @computer_rps)
+
+    render({ :template => "game_templates/play_rps" })
+  end
+
+  def scissors
+    @user_move = "scissors"
+    @computer_rps = RPS.sample
+    @rps_outcome = rps_game(@user_move, @computer_rps)
 
     render({ :template => "game_templates/play_rps" })
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get("/rock", { :controller => "rps", :action => "rock" })
 
-  get("/paper", { :controller => "rps", :action => "rock" })
+  get("/paper", { :controller => "rps", :action => "paper" })
 
   get("/scissors", { :controller => "rps", :action => "scissors" })
 end
